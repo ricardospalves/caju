1744fd33a09e1bbe710cf24c8b2b0f4b1c0f07a0
Ricardo Alves <ricardospalves@gmail.com>
2024-06-29 01:29:09 -0300

    fix: add index file to <Header />

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -1,5 +1,5 @@
 import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
-import { Header } from '~/Components/Header/Header'
+import { Header } from '~/Components/Header'
 
 const router = createBrowserRouter([
   {
